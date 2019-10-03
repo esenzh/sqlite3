3b773a159b83ebf1e15571d6fbd1db8e1c020d8f
Esen Zheenchoroev <stefunices@gmail.com>
2019-10-03 14:24:09 +0300

    feat: handle the sql injection

--- listOfContacts.py
+++ listOfContacts.py
@@ -2,11 +2,16 @@ import sqlite3
 
 db = sqlite3.connect('contacts.sqlite')
 
-for name, phone, email in db.execute("SELECT * FROM contacts"):
-    print(name)
-    print(phone)
-    print(email)
-    print('-' * 20)
+name = input('Please, enter the name: ')
 
+for i in db.execute("SELECT * FROM contacts WHERE name = ?", (name,)):
+    print(i)
+
+
+
+# for name, phone, email in db.execute("SELECT * FROM contacts"):
+#     print(name)
+#     print(phone)
+#     print(email)
 
 db.close()
